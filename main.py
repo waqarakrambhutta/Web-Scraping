165ca314fc38b5f3496a88a46bfd2bce1caea53b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-08-25 20:50:23 +0500

    Using beautiful soup.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://codewithmosh.com/'
 r = requests.get(url)
 
-print(r.text)
+# print(r.text)
+
+soup = BeautifulSoup(r.text,'lxml')
+print(soup)
+
