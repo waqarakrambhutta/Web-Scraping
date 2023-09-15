9433849748d74292760dfec2d170805408a0c597
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-09-15 23:09:47 +0500

    Scraping rating.

--- find.py
+++ find.py
@@ -6,4 +6,8 @@ url = 'https://webscraper.io/test-sites/e-commerce/allinone'
 r = requests.get(url)
 soup= BeautifulSoup(r.text,'lxml')
 
-print(soup.find('h4',{'class':'pull-right price'}).string)
+price = soup.find('h4',{'class':'pull-right price'})
+print(price.string)
+
+rating = soup.find('p',{'class':'pull-right'})
+print(rating.string)
