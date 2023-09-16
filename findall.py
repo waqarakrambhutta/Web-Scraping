c1ce7986f9a51c6bd3c2399e87fd0ee18edf7bc5
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-09-16 23:43:26 +0500

    scraping a all <p>

--- findall.py
+++ findall.py
@@ -6,7 +6,7 @@ url = "https://webscraper.io/test-sites/e-commerce/allinone/computers"
 r = requests.get(url)
 soup = BeautifulSoup(r.text,'lxml')
 
-prices = soup.find_all('h4',class_ = 'pull-right price')
+desc = soup.find_all('p', class_ = "description")
 
-for i in prices:
+for i in desc:
     print(i.string)
