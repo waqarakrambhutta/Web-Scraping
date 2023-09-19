aba3a3f1d8de0a5ab0189df12cad9a5f23578529
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-09-20 00:08:19 +0500

    extract data from nested html tag

--- nested.py
+++ nested.py
@@ -8,5 +8,7 @@ soup = BeautifulSoup(r.text,'lxml')
 
 box = soup.find_all('div',class_ ="col-sm-4 col-lg-4 col-md-4")
 
-for i in box:
-    print(i)
+for i in range(len(box)):
+    price = box[i].find('h4',class_ = 'pull-right price')
+    print(price.string)
+
